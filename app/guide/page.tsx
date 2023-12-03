acb887b02391dec6f455c82ada9e42d217c8d85f
AKK-soft-dev <aungkokosoftdev@gmail.com>
2023-12-03 11:52:03 +0630

    add usage guide page

---
+++ app/guide/page.tsx
@@ -0,0 +1,26 @@
+import Step from "../ui/step";
+
+const UsageGuidePage = () => {
+  return (
+    <section>
+      <h1 className="text-4xl font-extrabold title-font">How to use?</h1>
+      <ul className="my-16 space-y-10">
+        <Step step={1} description="Create an account." />
+        <Step step={2} description="Once you have an account, go to profile." />
+        <Step
+          step={3}
+          description={
+            <>
+              You will see two options to share your link. Choose any option,
+              share it on any social media platform, and wait for incoming
+              anonymous messages. Don&apos;t forget to put caption like
+              &quot;Tell me what you all feel&quot;.
+            </>
+          }
+        />
+      </ul>
+    </section>
+  );
+};
+
+export default UsageGuidePage;
---
+++ app/ui/step.tsx
@@ -0,0 +1,18 @@
+const Step = ({
+  step,
+  description,
+}: {
+  step: number;
+  description: React.ReactNode;
+}) => {
+  return (
+    <li className="relative px-5 py-7 bg-white shadow">
+      <span className="absolute left-5 top-0 -translate-y-1/2 bg-black text-white w-10 h-10 flex justify-center items-center rounded-full">
+        {step}
+      </span>
+      <p className="orange_gradient font-bold">{description}</p>
+    </li>
+  );
+};
+
+export default Step;
