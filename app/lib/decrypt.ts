6a93bce04bbf7ecf04fd89f32aa78d8fd30c2e33
AKK-soft-dev <aungkokosoftdev@gmail.com>
2023-12-12 23:42:50 +0630

    update decryption method

--- app/lib/decrypt.ts
+++ app/lib/decrypt.ts
@@ -1,11 +1,20 @@
 import * as crypto from "crypto";
 
+const secretKey: Buffer = crypto
+  .createHash("sha256")
+  .update(process.env.NEXT_PUBLIC_CRYPTO_SECRET!)
+  .digest();
+
 export default function decryptText(encryptedText: string) {
-  const decipher = crypto.createDecipher(
-    "aes-256-ctr",
-    process.env.NEXT_PUBLIC_CRYPTO_SECRET!
+  const textParts = encryptedText.split(":");
+  const iv = Buffer.from(textParts.shift()!, "hex");
+  const encryptedData = Buffer.from(textParts.join(":"), "hex");
+  let decipher = crypto.createDecipheriv(
+    "aes-256-cbc",
+    Buffer.from(secretKey),
+    iv
   );
-  let decrypted = decipher.update(encryptedText, "hex", "utf-8");
-  decrypted += decipher.final("utf-8");
-  return decrypted;
+  let decrypted = decipher.update(encryptedData);
+  decrypted = Buffer.concat([decrypted, decipher.final()]);
+  return decrypted.toString();
 }
